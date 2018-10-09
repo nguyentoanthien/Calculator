d0c08689a76597cf7787b5f419f329134d692cc9
HgMinhQuang <nkokhohoang1998@ou.edu.vn>
2018-10-09 14:21:17 +0700

    Them nut phep Tru va xu ly nut phep Tru

---
+++ Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = a - b;
+            textBox3.Text = c.ToString();
+        }
+    }
+}
